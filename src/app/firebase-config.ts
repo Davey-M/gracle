3d1649c8dd4aaa008c4b4576a764963de0453dd6
Davey-M <davey.meuer@gmail.com>
2023-08-12 21:55:09 -0500

    add firebase config

---
+++ src/app/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdzcP6vaYw0wyLlxCEFY-nHkvUNXTZRho",
+  authDomain: "gracle.firebaseapp.com",
+  projectId: "gracle",
+  storageBucket: "gracle.appspot.com",
+  messagingSenderId: "171859567688",
+  appId: "1:171859567688:web:982240efa52a7cd9e6647d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
