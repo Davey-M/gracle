9163b91c7e6a5d46b7dac8c829c13c3abaefed1a
Davey-M <davey.meuer@gmail.com>
2023-08-15 19:56:04 -0500

    add new format for copying results

--- src/app/components/output-display/output-display.component.ts
+++ src/app/components/output-display/output-display.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { map, tap } from 'rxjs';
-import { gracleState, iGracleTile } from 'src/app/models/gracle';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, combineLatest, map, skipWhile, takeUntil, tap } from 'rxjs';
+import { gracleState, iGracleTile, iRule } from 'src/app/models/gracle';
+import { RulesService } from 'src/app/services/rules/rules.service';
 import { StateService } from 'src/app/services/state/state.service';
 
 @Component({
@@ -8,7 +9,7 @@ import { StateService } from 'src/app/services/state/state.service';
   templateUrl: './output-display.component.html',
   styleUrls: ['./output-display.component.css']
 })
-export class OutputDisplayComponent implements OnInit {
+export class OutputDisplayComponent implements OnInit, OnDestroy {
 
   private _gracleResultString = '';
   gracleResultString$ = this._stateService.tileState$.pipe(
@@ -16,33 +17,64 @@ export class OutputDisplayComponent implements OnInit {
     tap(str => this._gracleResultString = str),
   );
 
-  constructor(private _stateService: StateService) { }
+  private _resultsCopyString = '';
+
+  private _unsubscribe$ = new Subject<null>();
+
+  constructor(private _stateService: StateService,
+              private _rulesService: RulesService) { }
 
   ngOnInit(): void {
+    combineLatest([
+      this._stateService.tileState$,
+      this._rulesService.rules$,      
+    ]).pipe(
+      takeUntil(this._unsubscribe$),
+      skipWhile(([tiles, rules]) => !tiles || !rules),
+    ).subscribe(([tiles, rules]) => {
+      this._setCopyString(tiles, rules);
+    })
+  }
+
+  ngOnDestroy(): void {
+      this._unsubscribe$.next(null);
   }
 
   private _formatStateToString(tiles: iGracleTile[]) {
-    let outputString = 'Gracle: ';
+    let outputString = '';
 
     for (let tile of tiles) {
-      switch (tile.state) {
-        case gracleState.inProgress:
-          outputString += '⬛';
-          break;
-        case gracleState.attempted:
-          outputString += '🟨';
-          break;
-        case gracleState.succeeded:
-          outputString += '🟩';
-          break;
-      }
+      outputString += this._getTileColor(tile.state);
     }
 
     return outputString;
   }
 
+  private _setCopyString(tiles: iGracleTile[], rules: iRule[]) {
+    const outputArray: string[] = [];
+
+    for (let tile of tiles) {
+      const rule = rules.find(r => r.index === tile.ruleIndex);
+
+      outputArray.push(`${this._getTileColor(tile.state)} ${rule?.summary}`);
+    }
+
+    this._resultsCopyString = outputArray.join('\n');
+  }
+
+  private _getTileColor(state: gracleState) {
+    switch (state) {
+      case gracleState.inProgress:
+        return '⬛';
+      case gracleState.attempted:
+        return '🟨';
+      case gracleState.succeeded:
+        return '🟩';
+    }
+  }
+
   async copyResults() {
-    await navigator.clipboard.writeText(this._gracleResultString);
+    await navigator.clipboard.writeText(this._resultsCopyString);
     alert('Results were copied to clipboard.');
   }
 
