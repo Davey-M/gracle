3fc29920141104981c78d88cf2f974a485c74f09
Davey-M <davey.meuer@gmail.com>
2023-09-01 22:32:49 -0500

    add ability to star a rule that you want to emphasize

--- src/app/components/rule-input/rule-input.component.ts
+++ src/app/components/rule-input/rule-input.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs';
-import { RulesService } from 'src/app/services/rules/rules.service';
+import { map, tap } from 'rxjs';
+import { RULES_VERSION, RulesService } from 'src/app/services/rules/rules.service';
 import { StateService } from 'src/app/services/state/state.service';
+import { StorageService } from 'src/app/services/storage/storage.service';
 
 @Component({
   selector: 'app-rule-input',
@@ -12,9 +13,24 @@ export class RuleInputComponent implements OnInit {
 
   currentRules$ = this._rulesService.rules$.asObservable();
 
+  private _starRuleIndex: number | null = null;
+  starRuleIndex$ = this._storageService.starRule$.pipe(
+    map(starRule => {
+      if (starRule?.version === RULES_VERSION) {
+        return starRule.index;
+      } else {
+        return null;
+      }
+    }),
+    tap(index => this._starRuleIndex = index),
+  );
+
+  private _setStarRuleTimeout: NodeJS.Timeout | null = null;
+
   constructor(
     private _rulesService: RulesService,
     private _stateService: StateService,
+    private _storageService: StorageService,
   ) { }
 
   ngOnInit(): void {
@@ -23,6 +39,24 @@ export class RuleInputComponent implements OnInit {
 
   updateTile(index: number) {
     this._stateService.updateTile(index);
+  }
+
+  handleRuleMouseDown(ruleIndex: number) {
+    this._setStarRuleTimeout = setTimeout(() => {
+      if (ruleIndex === this._starRuleIndex) {
+        this._storageService.setStarRule(null);
+      } else {
+        this._storageService.setStarRule(ruleIndex);
+      }
+
+      this._setStarRuleTimeout = null;
+    }, 500);
+  }
+
+  handleRuleMouseUp() {
+    if (this._setStarRuleTimeout) {
+      clearTimeout(this._setStarRuleTimeout);
+    }
   }
 
 }
---
+++ src/app/models/star-rule.ts
@@ -0,0 +1,4 @@
+export interface iStarRule {
+  version: string;
+  index: number;
+}
