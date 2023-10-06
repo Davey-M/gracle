4b33eeecbd79f63795a9a29dbe973e934a82fd9c
David <75805825+Davey-M@users.noreply.github.com>
2023-10-05 21:41:18 -0500

    Fix stats (#6)
    * fix widths on stats
    
    * add mobile friendly tooltip for stats percentage
    
    * small cleanup
    
    * remove unused imports

--- src/app/components/stats-main/stats-main.component.ts
+++ src/app/components/stats-main/stats-main.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, map, switchMap } from 'rxjs';
-import { gracleState, iGracle, iGracleTile } from 'src/app/models/gracle';
+import { Subject, debounceTime, map, merge } from 'rxjs';
+import { gracleState, iGracle } from 'src/app/models/gracle';
 import { iRuleStat } from 'src/app/models/stats';
-import { RulesService } from 'src/app/services/rules/rules.service';
 import { StorageService } from 'src/app/services/storage/storage.service';
 
 @Component({
@@ -15,6 +14,33 @@ export class StatsMainComponent implements OnInit {
   stats$ = this._storageService.store$.pipe(
     map(store => this._groupByRuleAndVersion(store)),
   );
+
+  mouseEvent$ = new Subject<MouseEvent>();
+
+  popupStartStyle$ = this.mouseEvent$.pipe(
+    map((e) => {
+      return {
+        'left.px': e.x,
+        'top.px': e.y,
+        'opacity': 1,
+      }
+    }),
+  );
+
+  popupEndStyle$ = this.mouseEvent$.pipe(
+    debounceTime(3_000), // 3 seconds
+    map((e) => {
+      return {
+        'left.px': e.x,
+        'top.px': e.y,
+        'opacity': 0,
+      }
+    }),
+  );
+
+  popupStyle$ = merge(this.popupStartStyle$, this.popupEndStyle$)
+
+  selectedPercent$ = new Subject<string>();
 
   constructor(private _storageService: StorageService) { }
 
@@ -83,10 +109,23 @@ export class StatsMainComponent implements OnInit {
       ruleStat.percentages.attempted = ruleStat.totals.attempted / total;
       ruleStat.percentages.succeeded = ruleStat.totals.succeeded / total;
 
+      ruleStat.style = { "grid-template-columns": `${ruleStat.totals.succeeded}fr ${ruleStat.totals.attempted}fr ${ruleStat.totals.missed}fr` };
+
       outputStats.push(ruleStat);
     }
 
     return outputStats;
   }
 
+  selectPercent(percent: number, event: MouseEvent) {
+    const percentString = `${(percent * 100).toFixed(2)}%`
+      // these splits/joins will remove any trailing 0's
+      .split('.00')
+      .join('')
+      .split('0%')
+      .join('%');
+
+    this.selectedPercent$.next(percentString);
+    this.mouseEvent$.next(event);
+  }
 }
